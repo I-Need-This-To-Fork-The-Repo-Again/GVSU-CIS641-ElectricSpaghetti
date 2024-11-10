3ef43292ae66c6defc03fd40b32ab3637b55e36f
Nick Carter <89876241+NickCarterGV@users.noreply.github.com>
2024-11-10 18:48:35 -0500

    Updated gameLogic.js
    Switched ideas, included experimental logic for each hit outcome. Changed the way teams are generated slightly.

--- src/around-the-horn/src/gameLogic.js
+++ src/around-the-horn/src/gameLogic.js
@@ -2,57 +2,88 @@ import GameplayMixin from '/GameplayMixin.js';
 
 const gameLogic = {
   mixins: [GameplayMixin],
-  data: () => ({
-    playerTeam: null,
-    opponentTeam: null,
-    game: null,
-    inventory: [], //player item inventory
-    mementos: [], //team mementos
-    currentTournament: 1,
-  }),
+  data: () {
+    playerTeam: {},
+    opponentTeam: {},
+    game: {
+      inning: 1,
+      top: true,
+      outs: 0,
+      baseStatus: {
+      first: false,
+      second: false,
+      third: false,
+    },
+    }
+    availableItems: [],
+    equippedItems: [],
+    availableMementos: [],
+    chosenMementos: [],
+    tournamentsWon: 0,
+  };
+},
+  
   methods: {
-    newGame() {
-      this.playerTeam = this.generateTeam();
-      this.startTournament: 1,
-    }),
-    startTournament() {
-      this.currentTournament++;
-      this.opponentTeam = this.generateTeam();
-      this.game = this.createGame(this.playerTeam, this.opponentTeam, true);
-    },
-
-    generateTeam() { //creates team with generated players
-      const team = {
-        players: [],
-        hitter: 0,
-        runs: 0,
-        hits: 0,
-        inningscore: [0, 0, 0],
-      };
+    generateTeam() { //creates team with generated players, all with stats between 50 and 100
+      const team = [];
       for (let i = 0; i < 9; i++) {
-        team.players.push(this.generatePlayer());
-      }
-      return team;
-    },
-
-    generatePlayer() { //sets player stats between 50 and 100, item is null at start
-      return {
+        team.push({
         contact: Math.floor(Math.random()*50) + 50,
         strength: Math.floor(Math.random()*50) + 50,
         vision: Math.floor(Math.random()*50) + 50,
         speed: Math.floor(Math.random()*50) + 50,
         luck: Math.floor(Math.random()*50) + 50,
-        item: null,
-      };
-    },
-
-    createGame(homeTeam, awayTeam, playerisHome) {
-      return {
-        
+        equippedItem: null,
+        });
       }
-    }
+      return {
+        players: team,
+        hitter: 0,
+        runs: 0,
+        hits: 0,
+        inningscore: [0, 0, 0],
+        home: false,
+      };
+},
+
+determineHitOutcome(batter) { //zero idea if this is gonna be balanced or not but we can figure that out later
+  let randomNumber = Math.random()*1000;
+  let contactMod = batter.contact + (batter.equippedItem ? batter.equippedItem.contactMod || 0 : 0);
+  let strengthMod = batter.strength + (batter.equippedItem ? batter.equippedItem.strengthMod || 0 : 0);
+  let visionMod = batter.vision + (batter.equippedItem ? batter.equippedItem.visionMod || 0 : 0);
+  let speedMod = batter.speed + (batter.equippedItem ? batter.equippedItem.speedMod || 0 : 0);
+  let luckMod = batter.luck + (batter.equippedItem ? batter.equippedItem.luckMod || 0 : 0);
+
+  if (randomNumber < (75 - (contactMod/2.5)) + (visionMod/15) + (luckMod/20)) { //number < 50 = strikeout
+    if (this.checkItemEffect(batter, "K")) {return}
+    return "K";
+  } else if (randomNumber < (250 - (contactMod/3)) + (visionMod/10) + (luckMod/20)) { //number < 200 = flyout
+    if (this.checkItemEffect(batter, "FO")) {return}
+    return "FO";
+  } else if (randomNumber < (350 - (contactMod/3)) + (visionMod/10) + (luckMod/20)) { //number < 300 = lineout
+    if (this.checkItemEffect(batter, "LO")) {return}
+    return "LO";
+  } else if (randomNumber < (600 - (contactMod/4)) + (visionMod/10) + (luckMod/20)) { //number < 600 = groundout
+    if (this.checkItemEffect(batter, "GO")) {return}
+    return "GO";
+  } else if (randomNumber < (800 + (strengthMod/8) - (visionMod/15) - (speedMod/12) + (luckMod/25))) { //number < 800 = single
+    if (this.checkItemEffect(batter, "1B")) {return}
+    return "1B";
+  } else if (randomNumber < (900 + (strengthMod/6) - (visionMod/15) + (speedMod/10) + (luckMod/25))) { //number < 900 = double
+    if (this.checkItemEffect(batter, "2B")) {return}
+    return "2B";
+  } else if (randomNumber < (950 + (strengthMod/5) - (visionMod/15) + (speedMod/8) + (luckMod/20))) { //number < 950 = triple
+    if (this.checkItemEffect(batter, "3B")) {return}
+    return "3B";
+  } else if (randomNumber < (995 + (strengthMod/4) - (visionMod/20) + (luckMod/18))) { //number < 975 = homer
+    if (this.checkItemEffect(batter, "HR")) {return}
+    return "HR";
+  } 
+  else {
+    if (this.checkItemEffect(batter, "BB")) {return}
+      return "BB"; //walk
   }
-}
+},
 
 
 
